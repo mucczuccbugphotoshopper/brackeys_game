02f78901a48c249f0854374a6c5c78ebd5f822b3
CMDR-Piboy314 <92357316+CMDR-Piboy314@users.noreply.github.com>
2022-02-24 09:30:21 +0900

    Fix Game-Breaking bugs, remove filter from all assets and fix spelling mistakes

--- flashlight.gd
+++ flashlight.gd
@@ -1,20 +1,11 @@
 extends Area2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play("item")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func _on_flashlight_body_entered(body):
-	queue_free()
-	body.light_mode = true
+	print(body.name)
+	if body.name == "Investigator":
+		body.light_mode = true
+		queue_free()
